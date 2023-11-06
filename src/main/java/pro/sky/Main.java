8009ba134836e3fbab08dcc4ef21e871ac48318b
bkvdm <vadim@991199.ru>
2023-11-06 15:41:11 +0300

    Лекционный материал раздела "Библиотеки": https://my.sky.pro/student-cabinet/stream-lesson/75302/theory/1/thread/104234

---
+++ src/main/java/pro/sky/Main.java
@@ -0,0 +1,15 @@
+package pro.sky;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
+//}
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("Вадим"));
+    }
+}
